735308d5595a52d7b44390ac47600f2caf913f5b
Mark Allen <markallen.webdeveloper@gmail.com>
2019-12-22 00:03:37 +0000

    Basic 'goals' app functionality in place

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          width="80%"
+          placeholder="Enter course goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map(goal => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    marginVertical: 5,
+    padding: 10,
+    backgroundColor: '#eee',
+    borderColor: '#aaa',
+    borderWidth: 1,
+    borderStyle: 'dotted'
+  }
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+# rn-second-app
+
+This is a simple React Native App built using Expo. It's my second React Native app put together following Max Swarzmuller's course "React Native - The Practical Guide" on Udemy.
+
+The app is a simple 'todo' list builder, in this case focussed on learning goals while taking the React Native course
+
+M Allen
+Dec 2019
